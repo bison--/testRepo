18ce6038d901fe9dce700aa91e96e8f779683c74
bison-- <mike.bison42@googlemail.com>
2013-04-29 18:54:58 +0200

    reanimation

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 
 # base class
 class deathInc(object):
+
+	def __init__(self):
+		self._state = None
+	
 	def printState(self):
-		print "dead body class"
+		if self._state == None:
+			print "dead body class"
+		else:
+			print self._state
 		
 	def setState(self, newState):
 		self._state = newState
